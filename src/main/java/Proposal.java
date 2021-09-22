37ee4b3679ce00295a6052d282c2c29541511b36
Jeanzin67 <jeanpcordova18@gmail.com>
2021-09-22 12:00:34 -0500

    Aplicacion1

---
+++ src/main/java/Proposal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author CORDOVA
+ */
+public class Proposal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    View view = new View ();
+    view.setVisible(true);
+
+    }
+    
+}
